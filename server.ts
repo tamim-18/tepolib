6b28e2fe672f70f247d93262fffcae082c69a812
tamim-18 <tamim11903060@gmail.com>
2024-05-31 21:13:32 +0600

    express setup done

--- server.ts
+++ server.ts
@@ -1,3 +1,10 @@
 import app from "./src/app";
 
 //seting up the server
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+startServer();
--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,11 @@
 import express from "express";
 
 const app = express();
+//routes
+//http methods are:
+//GET, POST, PUT, DELETE
+app.get("/", (req, res, next) => {
+  res.json({ message: "Hello World" });
+});
 
 export default app;
