a26c3add326451b62e0fa60d0bd97dba59780543
tamim-18 <tamim11903060@gmail.com>
2024-06-04 11:52:31 +0600

    modify book api

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.get("/", (req, res, next) => {
 //registering the user router
 
 app.use("/api/users", userRouter);
-app.use("api/books", bookRouter);
+app.use("/api/books", bookRouter);
 
 // global error handler
 // it should be the last middleware. otherwise, it will not catch errors
