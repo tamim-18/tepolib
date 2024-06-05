59790e50ea8cc9560c956b4254471963c3e3cc7b
tamim-18 <tamim11903060@gmail.com>
2024-06-05 16:58:58 +0600

    setup multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,12 +1,29 @@
 // we have to make the user router
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "node:path/posix";
 
 const bookRouter = express.Router();
+// multer is a middleware for handling multipart/form-data, which is primarily used for uploading files.
+
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  // dest is the destination where the file will be uploaded
+  // __dirname is the directory name of the current module
+  limits: { fileSize: 3e7 },
+});
 
 // creating the routes
 
-bookRouter.post("/", createBook);
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 // why are we using createrUser instead of createrUser()?
 // because we are passing the reference of the function to the post method.
