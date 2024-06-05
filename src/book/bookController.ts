342c647bc5d6cfcf3710c3847862e66fe09dafc5
tamim-18 <tamim11903060@gmail.com>
2024-06-05 21:53:09 +0600

    multer tested

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  console.log("files", req.files);
   res.json({ message: "Book created" });
 };
 
