5ec3fae542774bd6609bfc863c8295cdaf5461e7
tamim-18 <tamim11903060@gmail.com>
2024-06-06 10:57:49 +0600

    add cloudinary for uploading

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,38 @@
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary_config";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   console.log("files", req.files);
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] }; // Type assertion
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/")[-1];
+  const filePath = files.coverImage[0].path;
+  const filename = files.coverImage[0].filename;
+
+  const bookFileName = files.file[0].filename;
+  const bookFilePath = files.file[0].path;
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: filename, // This is the name of the file on Cloudinary
+      folder: "book-covers", // This is the folder where the file will be stored on Cloudinary
+      format: coverImageMimeType, // This is the format of the file
+    });
+
+    const bookUploadResult = await cloudinary.uploader.upload(bookFilePath, {
+      resource_type: "raw", // This is the type of the file.
+      filename_override: bookFileName,
+      folder: "books-pdf",
+      format: "pdf",
+    });
+    console.log("uploadResult", uploadResult);
+    console.log("BookuploadResult", bookUploadResult);
+  } catch (err) {
+    console.log(err);
+    return next(createHttpError(500, "Error while uploading file"));
+  }
+
+  // console.log("uploadResult", uploadResult);
   res.json({ message: "Book created" });
 };
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,9 @@ const _config = {
   dburl: process.env.MONGODB_URI,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 export const config = Object.freeze(_config);
 //object.freeze() is used to make the object immutable. This means that the object cannot be modified in any way. This is useful when you want to ensure that the object is not changed by any other part of the code.
